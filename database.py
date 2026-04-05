ac3357157deb62582e8f657430901d4e2ad4f9ee
Omar Zayed Beshir <omarzayed7777@gmail.com>
2026-04-05 14:12:40 +0200

    Updated SSL params

--- database.py
+++ database.py
@@ -2,12 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
+import ssl
+import base64
+import tempfile
+
+ca_cert_b64 = os.environ["DB_CA_CERT"]
+ca_cert_pem = base64.b64decode(ca_cert_b64)
+
+with tempfile.NamedTemporaryFile(delete=False, suffix=".pem") as f:
+    f.write(ca_cert_pem)
+    ca_cert_path = f.name
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=False, connect_args={"ssl": True})
+engine = create_engine(DATABASE_URL, echo=False, connect_args={"ssl": {"ca": ca_cert_path}})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
